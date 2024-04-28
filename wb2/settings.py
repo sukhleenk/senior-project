cfb0cfee00c4c7141bbf7e979c3c02326d9b2224
Soumil Datta <soumildatta@gmail.com>
2024-04-28 00:37:41 -0500

    Another attempt at static files

--- wb2/settings.py
+++ wb2/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 
 # STATIC_ROOT = 'C:/Users/sukh1/Downloads/Senior_project_jewelry'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
